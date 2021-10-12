f171fa0b1c30a27bc59dc7d8e82c4da2bca25c99
Maximiliano <maxirpc2607@gmail.com>
2021-10-12 15:42:01 -0300

    install express

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-console.log("hola mundo");
+const { response } = require("express");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 let notes = [
   {
@@ -20,3 +24,16 @@ let notes = [
     important: true,
   },
 ];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hola Mundo</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
